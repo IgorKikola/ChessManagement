1cbf78b80d5551bd1fa293ff8c1ad6dfacba6b68
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2021-11-14 17:44:34 +0200

    Update login page with bootstrap

--- ChessClub/settings.py
+++ ChessClub/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'chessManagement',
 ]
 
