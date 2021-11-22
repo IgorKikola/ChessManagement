9129bf87fc472155ea448b8e0a50ca7d50417898
Igor Jasutowicz <k20069609@kcl.ac.uk>
2021-11-22 07:16:45 +0000

    Add applicant list only available to officer rank and above

--- ChessClub/urls.py
+++ ChessClub/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('log_out/', views.log_out, name='log_out'),
     path('user/<int:user_id>/', views.show_user, name='show_user'),
     path('users/', views.user_list, name='user_list'),
+    path('applicants/', views.applicant_list, name='applicant_list'),    
     path('user/<int:user_id>/to_member/', views.to_member, name='to_member'),
     path('user/<int:user_id>/to_officer/', views.to_officer, name='to_officer')
 ]
--- chessManagement/views.py
+++ chessManagement/views.py
@@ -90,6 +90,15 @@ def user_list(request):
         return render(request, 'user_list.html', {'users': users})
 
 @login_required
+def applicant_list(request):
+    user = request.user
+    if user.user_level == 0 or user.user_level == 1:
+        return render(request, 'page_unavailable.html')
+    else:
+        users = User.objects.all()
+        return render(request, 'applicant_list.html', {'users': users})
+
+@login_required
 def show_user(request, user_id):
     try:
         user = User.objects.get(id=user_id)
