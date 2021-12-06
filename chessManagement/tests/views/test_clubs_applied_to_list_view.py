7efa2a48da539c151ef036768c941cdc6e835ff0
Zena Wang <zena.Wang@kcl.ac.uk>
2021-12-06 22:18:08 +0800

    add test_club_applied_to_list_view

--- chessManagement/tests/views/test_clubs_applied_to_list_view.py
+++ chessManagement/tests/views/test_clubs_applied_to_list_view.py
@@ -1,32 +1,36 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import authenticate,login
-from chessManagement.models import User, Club
+from chessManagement.models import User, Club, UserInClub
 from chessManagement.tests.helpers import reverse_with_next
 from chessManagement.forms import createClubForm
 
-class ClubListTest(TestCase):
+class clubsAppliedToListTest(TestCase):
 
-    fixtures = ['chessManagement/tests/fixtures/default_user.json']
+    fixtures = [
+        'chessManagement/tests/fixtures/default_user.json',
+        'chessManagement/tests/fixtures/other_users.json',
+    ]
 
     def setUp(self):
         self.user = User.objects.get(username='johndoe@example.org')
-        self.url = reverse('club_list')
+        self.target_user = User.objects.get(username='janedoe@example.org')
+        self.url = reverse('clubs_applied_to_list')
 
     def test_club_list_url(self):
-        self.assertEqual(self.url,'/clubs/')
+        self.assertEqual(self.url,'/clubs/applied/')
 
-    def test_get_club_list_redirects_when_not_logged_in(self):
+    def test_get_club_applied_to_list_redirects_when_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    def test_get_club_list(self):
+    def test_get_clubs_applied_to_list(self):
         self.client.login(username=self.user.email, password='Password123')
         self._create_test_clubs(15)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'club_list/all.html')
+        self.assertTemplateUsed(response, 'club_list/applied_to.html')
         self.assertEqual(len(response.context['clubs']), 15)
         for i in range(15):
             club = Club.objects.get(name=f'Club{i}')
@@ -35,11 +39,20 @@ class ClubListTest(TestCase):
 
     def _create_test_clubs(self, club_count):
         for i in range(club_count):
-            form_input = {
-                'name':f'Club{i}',
-                'location':f'Location{i}',
-                'description':f'Description{i}',
-            }
+            club = Club.objects.create(
+                name=f'Club{i}',
+                location=f'Location{i}',
+                description=f'Description{i}'
+            )
 
-            form = createClubForm(data=form_input)
-            form.save(self.user)
+            UserInClub.objects.create(
+                user=self.user,
+                club=club,
+                user_level=0
+            )
+
+            UserInClub.objects.create(
+                user=self.target_user,
+                club=club,
+                user_level=3
+            )
