9ff7eb51a6ad5c3934e358467e070435c008b8cb
Zena Wang <zena.Wang@kcl.ac.uk>
2021-12-06 11:08:42 +0800

    add tests for test_show_user_view.py

--- chessManagement/tests/views/test_show_user_view.py
+++ chessManagement/tests/views/test_show_user_view.py
@@ -14,6 +14,7 @@ class ShowUserTest(TestCase):
         self.user = User.objects.get(username='johndoe@example.org')
         self.target_user = User.objects.get(username='janedoe@example.org')
         self.user_not_in_club = User.objects.get(username='peterpickles@example.org')
+        self.officer_user = User.objects.get(username='petrapickles@example.org')
         self.club = Club.objects.get(name='KCL-Chess-Society')
 
 
@@ -28,6 +29,12 @@ class ShowUserTest(TestCase):
             user=self.target_user,
             club=self.club,
             user_level=1
+        )
+
+        officer_user_in_club = UserInClub.objects.create(
+            user=self.officer_user,
+            club=self.club,
+            user_level=2
         )
 
         self.url = reverse('show_user', kwargs={'club_pk': self.club.pk, 'user_id': self.target_user.id})
@@ -50,5 +57,26 @@ class ShowUserTest(TestCase):
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'club_list/all.html')
 
-    """Should write tests for test_get_show_user_with_own_id"""
-    """Need to create Club and UserInClub in these tests"""
+    def test_get_show_user_with_own_id(self):
+        self.client.login(username=self.user.email, password='Password123')
+        url = reverse('show_user', kwargs={'club_pk': self.club.pk, 'user_id': self.user.id})
+        response = self.client.get(url, follow=True)
+        response_url = reverse('profile')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'profile.html')
+
+    def test_owner_get_show_user_which_is_in_the_club_and_with_valid_id(self):
+        self.client.login(username=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'show_member/for_owner.html')
+        self.assertContains(response, "Jane Doe")
+        self.assertContains(response, "janedoe@example.org")
+
+    def test_officer_get_show_user_which_is_in_the_club_and_with_valid_id(self):
+        self.client.login(username=self.officer_user.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'show_member/for_officer.html')
+        self.assertContains(response, "Petra Pickles")
+        self.assertContains(response, "petrapickles@example.org")
