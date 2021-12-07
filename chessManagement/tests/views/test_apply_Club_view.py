5958e0aee9c9bcb05b31d66748ad6e53e45ed88f
Zena Wang <zena.Wang@kcl.ac.uk>
2021-12-08 00:18:37 +0800

    refine test_apply_Club_view

--- chessManagement/tests/views/test_apply_Club_view.py
+++ chessManagement/tests/views/test_apply_Club_view.py
@@ -19,7 +19,7 @@ class ApplyClubViewTestCase(TestCase):
         self.owner_user = User.objects.get(username='janedoe@example.org')
         self.club = Club.objects.get(name='KCL-Chess-Society')
 
-        officer_user_in_club = UserInClub.objects.create(
+        owner_user_in_club = UserInClub.objects.create(
             user=self.owner_user,
             club=self.club,
             user_level=3
@@ -38,33 +38,28 @@ class ApplyClubViewTestCase(TestCase):
     def test_succesful_apply_to(self):
         self.client.login(username='johndoe@example.org', password='Password123')
         before_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
-        officer_user_in_club = UserInClub.objects.create(
-            user=self.user,
-            club=self.club,
-            user_level=0
-        )
-        after_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
-        self.assertEqual(after_count, before_count+1)
         response = self.client.post(self.url, follow=True)
         response_url = reverse('show_club', kwargs={'club_pk': self.club.pk})
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        after_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
+        self.assertEqual(after_count, before_count+1)
         self.assertTemplateUsed(response, 'show_club/for_applicant.html')
         club = Club.objects.get(pk=self.club.pk)
         self.assertTrue(club.name == "KCL-Chess-Society")
 
     def test_already_apply_to(self):
         self.client.login(username='johndoe@example.org', password='Password123')
-        officer_user_in_club = UserInClub.objects.create(
+        UserInClub.objects.create(
             user=self.user,
             club=self.club,
             user_level=0
         )
         before_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
-        after_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
-        self.assertEqual(after_count, before_count)
         response = self.client.post(self.url, follow=True)
         response_url = reverse('show_club', kwargs={'club_pk': self.club.pk})
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'show_club/for_applicant.html')
+        after_count = len(UserInClub.objects.filter(club=self.club, user=self.user))
+        self.assertEqual(after_count, before_count)
         club = Club.objects.get(pk=self.club.pk)
         self.assertTrue(club.name == "KCL-Chess-Society")
