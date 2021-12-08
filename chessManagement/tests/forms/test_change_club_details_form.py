21043da3dd50e89cd3235d6b7cf60d6757ff8f82
Zena Wang <zena.Wang@kcl.ac.uk>
2021-12-08 18:06:36 +0800

    test_change_club_details_form

---
+++ chessManagement/tests/forms/test_change_club_details_form.py
@@ -0,0 +1,29 @@
+
+"""Unit tests of the log in form."""
+from django import forms
+from django.test import TestCase
+from chessManagement.forms import changeClubDetails
+
+
+class ChangePasswordTestCase(TestCase):
+    """Unit tests of the log in form."""
+    def setUp(self):
+        self.form_input = {
+            'location': 'ClubLocation',
+            'description': 'ClubDescription'
+        }
+
+    def test_form_contains_required_fields(self):
+        form = changeClubDetails()
+        self.assertIn('location', form.fields)
+        self.assertIn('description', form.fields)
+
+    def test_form_rejects_blank_location(self):
+        self.form_input['location'] = ''
+        form = changeClubDetails(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_form_accepts_blank_description(self):
+        self.form_input['description'] = ''
+        form = changeClubDetails(data=self.form_input)
+        self.assertTrue(form.is_valid())
