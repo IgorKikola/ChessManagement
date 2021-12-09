82d0c284c0c32313c666d57303a4810823a5a815
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2021-12-09 19:46:57 +0200

    Add valid_club_required wrapper

--- chessManagement/helpers.py
+++ chessManagement/helpers.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Club
 
 def login_prohibited(view_function):
     def modified_view_function(request):
@@ -7,4 +9,14 @@ def login_prohibited(view_function):
             return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
         else:
             return view_function(request)
+    return modified_view_function
+
+def valid_club_required(view_function):
+    def modified_view_function(request, club_pk, *args):
+        try:
+            club = Club.objects.get(pk=club_pk)
+        except ObjectDoesNotExist:
+            return redirect('club_list')
+        else:
+            return view_function(request, club_pk, *args)
     return modified_view_function
