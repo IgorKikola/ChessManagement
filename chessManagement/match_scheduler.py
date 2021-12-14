8e619ae1b3b5440c1a919a92b59f7f304f2dce52
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2021-12-14 20:17:47 +0200

    Complete match scheduler

--- chessManagement/match_scheduler.py
+++ chessManagement/match_scheduler.py
@@ -1,5 +1,50 @@
 """ Schedules games in tournaments """
 
+
+def schedule(players):
+    matches = []
+    number_of_players = len(players)
+    if number_of_players > 16: # group stage
+        if number_of_players > 32:
+            groups = assign_to_groups(players, 6)
+        else:
+            groups = assign_to_groups(players, 4)
+        for group in groups:
+            matches.extend(schedule_within_group(group))
+    else: # main bracket
+        if number_of_players in [2,4,8,16]:
+            match_all(players)
+        else: # elimination stage is needed
+            if number_of_players > 6:
+                groups = assign_to_groups(players, 4)
+            else:
+                groups = assign_to_groups(players, 6)
+            for group in groups:
+                matches.extend(schedule_within_group(group))
+    return matches
+
+
+def reorder(list):
+    new_list = []
+    i = 0
+    l = len(list)
+    while i < int(l/2):
+        new_list.append(list[i])
+        i = i + 1
+        new_list.append(list[l-i])
+    return new_list
+
+
+def match_all(players):
+    matches = []
+    i = 0
+    l = len(players)
+    if l % 2 == 0:
+        while i < l:
+            matches.append((players[i], players[i + 1]))
+            i = i + 2
+    return matches
+
 def assign_to_groups(players, group_size):
     number_of_players = len(players)
     number_of_groups = int(number_of_players / group_size)
@@ -15,42 +60,42 @@ def assign_to_groups(players, group_size):
         extra_group = []
         for i in range(group_size * number_of_groups, number_of_players):
             extra_group.append(players[i])
-        if len(extra_group) < 3:
+        if len(extra_group) < 3  and len(groups) > 1:
             extra_group.append(groups[0].pop())
             extra_group.append(groups[1].pop())
         groups.append(extra_group)
     return groups
 
-def schedule(teams):
-    """Return the list of games."""
-    if len(teams) % 2 == 0:
-        return schedule_even_draw(teams)
-    else:
-        return schedule_odd_draw(teams)
 
-def schedule_even_draw(teams):
+def schedule_within_group(group):
+    if len(group) % 2 == 0:
+        return schedule_even_draw(group)
+    else:
+        return schedule_odd_draw(group)
+
+def schedule_even_draw(group):
     """Return the list of games."""
     matches = []
-    half_len = int(len(teams)/2)
+    half_len = int(len(group)/2)
     arr1 = [i+1 for i in range(half_len)]
-    arr2 = [i+1 for i in range(half_len, len(teams))][::-1]
-    for i in range(len(teams)-1):
+    arr2 = [i+1 for i in range(half_len, len(group))][::-1]
+    for i in range(len(group)-1):
         arr1.insert(1, arr2.pop(0))
         arr2.append(arr1.pop())
         for gm in zip(arr1, arr2):
-            matches.append((teams[gm[0]-1], teams[gm[1]-1]))
+            matches.append((group[gm[0]-1], group[gm[1]-1]))
     return matches
 
-def schedule_odd_draw(teams):
+def schedule_odd_draw(group):
     """Return the list of games."""
     matches = []
-    half_len = int((len(teams)+1)/2)
+    half_len = int((len(group)+1)/2)
     arr1 = [i+1 for i in range(half_len)]
-    arr2 = [i+1 for i in range(half_len, len(teams)+1)][::-1]
-    for i in range(len(teams)):
+    arr2 = [i+1 for i in range(half_len, len(group)+1)][::-1]
+    for i in range(len(group)):
         arr1.insert(1, arr2.pop(0))
         arr2.append(arr1.pop())
         for gm in zip(arr1, arr2):
-            if len(teams)+1 not in gm:
-                matches.append((teams[gm[0]-1], teams[gm[1]-1]))
+            if len(group)+1 not in gm:
+                matches.append((group[gm[0]-1], group[gm[1]-1]))
     return matches
