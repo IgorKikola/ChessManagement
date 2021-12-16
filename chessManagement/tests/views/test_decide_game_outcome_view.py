60a9fa3c3cc7e2c70034e63a91d35f4c750b07ae
Flavio Melinte Citea <flavio.melinte_citea@kcl.ac.uk>
2021-12-16 22:21:54 +0200

    Add tests for decide_game_outcome view

--- chessManagement/tests/views/test_decide_game_outcome_view.py
+++ chessManagement/tests/views/test_decide_game_outcome_view.py
@@ -3,11 +3,11 @@ import datetime
 from django.test import TestCase
 from django.urls import reverse
 from chessManagement.forms import createClubForm, createTournamentForm
-from chessManagement.models import User, Club, Tournament, UserInClub, UserInTournament
+from chessManagement.models import User, Club, Tournament, UserInClub, UserInTournament, Game
 from chessManagement.tests.helpers import reverse_with_next
 
 
-class ShowMatchesViewTestCase(TestCase):
+class DecideGameOutcomeTestCase(TestCase):
 
 
     fixtures = [
@@ -20,6 +20,7 @@ class ShowMatchesViewTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.get(username='johndoe@example.org')
         self.member_user = User.objects.get(username='janedoe@example.org')
+        self.other_member = User.objects.get(username='peterpickles@example.org')
         self.club = Club.objects.get(name='KCL-Chess-Society')
         self.tournament = Tournament.objects.create(
             name="John Tournament",
@@ -46,24 +47,45 @@ class ShowMatchesViewTestCase(TestCase):
             user_level=1,
             club=self.club
         )
-        self.url = reverse('show_matches', kwargs={'club_pk': self.club.pk,'tournament_pk':self.tournament.pk})
+        UserInClub.objects.create(
+            user=self.other_member,
+            user_level=1,
+            club=self.club
+        )
+        self.game = Game.objects.create(
+            player1 = self.member_user,
+            player2 = self.other_member,
+            tournament = self.tournament,
+            finished = False,
+        )
+        self.url = reverse('decide_game_outcome', kwargs={'club_pk': self.club.pk,'tournament_pk':self.tournament.pk,'game_pk': self.game.pk})
 
-    def test_get_show_matches_url(self):
-        self.assertEqual(self.url,f'/club/{self.club.pk}/tournament/{self.tournament.pk}/matches/')
+    def test_get_decide_game_outcome_url(self):
+        self.assertEqual(self.url,f'/club/{self.club.pk}/tournament/{self.tournament.pk}/match/{self.game.pk}')
 
-    def test_get_show_matches(self):
+    def test_get_decide_game_outcome(self):
         self.client.login(username='johndoe@example.org', password='Password123')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'show_scheduled_matches/for_organisers.html')
+        self.assertTemplateUsed(response, 'decide_game_outcome.html')
 
-    def test_get_show_matches_by_member_user(self):
+    def test_cannot_be_accessed_by_member(self):
         self.client.login(username=self.member_user.email, password='Password123')
+        redirect_url = reverse('show_matches', kwargs={'club_pk': self.club.pk,'tournament_pk':self.tournament.pk})
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'show_scheduled_matches/for_members.html')
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_redirects_when_not_logged_in(self):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_succesful_entering_outcome(self):
+        self.client.login(username='johndoe@example.org', password='Password123')
+        form_input = { 'winner': "1" }
+        response = self.client.post(self.url, form_input, follow=True)
+        game = Game.objects.get(pk=self.game.pk)
+        self.assertEqual(game.winner, "1")
+        self.assertTrue(game.isFinished())
+        redirect_url = reverse('show_matches', kwargs={'club_pk': self.club.pk,'tournament_pk':self.tournament.pk})
+        self.assertRedirects(response, redirect_url)
